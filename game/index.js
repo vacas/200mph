f5f9f7d38df5f56bf6a3d6bfd9b7653b2f92bd5f
Miguel <m.vacas@agenciachat.com>
2019-05-04 14:32:32 -0400

    Progress with tilemaps and tilesets

--- game/index.js
+++ game/index.js
@@ -1,38 +1,106 @@
 // import 'pixi';
 // import 'p2';
-import Phaser from 'phaser';
-import vader from './assets/darth_vader.png';
-import empire from './assets/empire.png';
-import blue from './assets/blueline.png';
-import darkblue from './assets/darkblueline.png';
+import Phaser     from 'phaser';
+// import vader      from './assets/darth_vader.png';
+import empire     from './assets/empire.png';
+// import blue       from './assets/blueline.png';
+// import darkblue   from './assets/darkblueline.png';
+import tileimages from './assets/tileimages.png';
+import tilemap    from './tilemap.json';
 
-let cursor, right, player, lanes = [], i = 0;
+let controls, cursor, right, player, lanes = [], i = 0;
 
 function preload() {
     this.load.image('vader', empire);
-    this.load.image('blue', blue);
-    this.load.image('darkblue', darkblue);
+    // this.load.image('blue', blue);
+    // this.load.image('darkblue', darkblue);
+    this.load.image('tileimages', tileimages);
+    this.load.tilemapTiledJSON('map', tilemap);
+    // this.load.image("tiles", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilesets/tuxmon-sample-32px-extruded.png");
+    // this.load.tilemapTiledJSON("map", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/tuxemon-town.json");
 }
 
 function create() {
-    var map = this.make.tilemap({ key: 'map' });
-    console.log(this);
-    debugger;
-    var lanesGroup = map.add.staticGroup('media/lane.png', 'blue');
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('tileimages', 'tileimages');
+    // debugger;
 
-    lanes[0] = lanesGroup.create(500, 557, 'blue');
-    lanes[1] = lanesGroup.create(500, 569, 'darkblue');
-    lanes[2] = lanesGroup.create(500, 581, 'blue');
-    lanes[3] = lanesGroup.create(500, 593, 'darkblue');
+    // console.log('tileset: ', tileset);
+    // const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+    // const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+    // const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
 
-    this.physics.world.gravity.y = 200;
-    player = this.physics.add.sprite(100, 350, 'vader');
-    cursor = this.input.keyboard.createCursorKeys();
+    // Parameters: layer name (or index) from Tiled, tileset, x, y
+    const collisionTop = map.createStaticLayer('collisionTop', tileset, 0, 0);
+    const collisionBottom = map.createStaticLayer('collisionBottom', tileset, 0, 0);
+    const ocean = map.createStaticLayer('ocean', tileset, 0, 0);
 
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+    collisionTop.setCollisionByProperty({ collides: true });
+    collisionBottom.setCollisionByProperty({ collides: true });
+    // ocean.setCollisionByProperty({ collides: true });
 
-    this.physics.add.collider(player, lanes);
+    // console.log(collisionBottom);
+
+    // console.log('ocean: ', ocean);
+    // console.log('collisionTop: ', collisionTop);
+    // console.log('collisionBottom: ', collisionBottom);
+
+    // Phaser supports multiple cameras, but you can access the default camera like this:
+  const camera = this.cameras.main;
+
+  // Set up the arrows to control the camera
+  cursor = this.input.keyboard.createCursorKeys();
+  controls = new Phaser.Cameras.Controls.FixedKeyControl({
+    camera: camera,
+    left: cursor.left,
+    right: cursor.right,
+    up: cursor.up,
+    down: cursor.down,
+    speed: 0.5
+  });
+
+  // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    // var lanesGroup = this.physics.add.staticGroup();
+    //
+    // lanes[0] = lanesGroup.create(500, 557, 'blue');
+    // lanes[1] = lanesGroup.create(500, 569, 'darkblue');
+    // lanes[2] = lanesGroup.create(500, 581, 'blue');
+    // lanes[3] = lanesGroup.create(500, 593, 'darkblue');
+
+    // this.physics.world.gravity.y = 200;
+    player = this.physics.add.sprite(100, 0, 'vader').setSize(30, 40);
+    // cursor = this.input.keyboard.createCursorKeys();
+    //
+    // player.setBounce(0.2);
+    // player.setCollideWorldBounds(true);
+    // this.physics.add.collider(player, collisionTop);
+    var topCollision = this.physics.add.collider(player, collisionTop);
+    this.physics.add.collider(player, collisionBottom);
+    //
+    // this.physics.add.collider(player, lanes);
+
+    this.input.keyboard.once("keydown_M", event => {
+      // this.physics.world.colliders.getActive().find((i) => { console.log(i); });
+      this.physics.world.removeCollider(topCollision);
+    })
+
+    this.input.keyboard.once("keydown_D", event => {
+    // Turn on physics debugging to show player's hitbox
+    this.physics.world.createDebugGraphic();
+
+    // Create worldLayer collision graphic above the player, but below the help text
+    const graphics = this.add
+      .graphics()
+      .setAlpha(0.75)
+      .setDepth(20);
+    ocean.renderDebug(graphics, {
+      tileColor: null, // Color of non-colliding tiles
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    });
+  });
 }
 
 function update() {
@@ -73,18 +141,19 @@ function move() {
 
 const config = {
     type: Phaser.WEBGL,
-    width: 800,
-    height: 600,
     parent: 'phaser-example',
     backgroundColor: '#fff',
     pixelArt: true,
     physics: {
-      default: 'impact',
+      default: 'arcade',
       arcade: {
         // debug: true,
         gravity: { y: 60 }
       }
     },
+    width: 720,
+    height: 360,
+    pixelArt: true,
     scene: {
         preload: preload,
         create: create,
---
+++ game/tiles/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tilemap" tilewidth="72" tileheight="72" tilecount="210" columns="30">
+ <image source="../tileimages.png" width="2178" height="572"/>
+</tileset>
